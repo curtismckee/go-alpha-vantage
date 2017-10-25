3cbdd052e7d875a1414c4310e49ea35c8c51d203
Curtis Mckee <cmckee.dev@gmail.com>
2017-10-25 15:48:54 -0600

    Adding crypto currency daily endpoint

---
+++ crypto/daily.go
@@ -0,0 +1,42 @@
+package crypto // alpha vantage
+
+import (
+	"fmt"
+	"net/http"
+
+	"../av"
+)
+
+type dailyConfig struct {
+	market string
+}
+
+type dailyOption func(opt *dailyConfig) error
+
+func SetDailyMarket(s string) dailyOption {
+
+	return func(config *dailyConfig) error {
+		config.market = s
+		return nil
+	}
+}
+
+func Daily(symbol string, apikey string, opts ...dailyOption) (*http.Response, error) {
+
+	defaultOptions := &dailyConfig{
+		market: "BTC",
+	}
+
+	for _, opt := range opts {
+		opt(defaultOptions)
+	}
+
+	url := fmt.Sprintf("%s/query?function=DIGITAL_CURRENCY_DAILY&symbol=%s&apikey=%s&market=%s",
+		av.AV_BASE_URL,
+		symbol,
+		apikey,
+		defaultOptions.market)
+
+	resp, err := http.Get(url)
+	return resp, err
+}
