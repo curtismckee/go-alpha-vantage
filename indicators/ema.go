58dd21670b2d9d85b84458b9fc58d5a3c14ae7b7
Curtis Mckee <cmckee.dev@gmail.com>
2017-10-25 22:03:04 -0600

    Adding technical indicators, estimated moving average endpoint

---
+++ indicators/ema.go
@@ -0,0 +1,22 @@
+package indicators // alpha vantage technical indicators
+
+import (
+	"fmt"
+	"net/http"
+
+	"../av"
+)
+
+func EMA(symbol string, apikey string, interval string, timePeriod string, seriesType string) (*http.Response, error) {
+
+	url := fmt.Sprintf("%s/query?function=EMA&symbol=%s&apikey=%s&interval=%s&time_period=%s&series_type=%s",
+		av.AV_BASE_URL,
+		symbol,
+		apikey,
+		interval,
+		timePeriod,
+		seriesType)
+
+	resp, err := http.Get(url)
+	return resp, err
+}
