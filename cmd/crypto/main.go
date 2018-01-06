7a6cb3e7f56de7d61bd01691a669c4ee040483d2
Evan Leis <evan.explodes@gmail.com>
2018-01-05 21:10:39 -0800

    [crypto] make the crypto command line cooler

--- cmd/crypto/main.go
+++ cmd/crypto/main.go
@@ -4,40 +4,47 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
+	"strings"
+	"time"
 
 	"github.com/cmckee-dev/go-alpha-vantage"
 )
 
+const (
+	timeFormat = "2006/01/02 3:04pm"
+)
+
 var (
 	apiKey           = flag.String("apikey", "", "api key for alpha vantage")
-	cryptoSymbol     = flag.String("crypto", "ETH", "crypto-currency to query")
+	cryptoSymbols    = flag.String("crypto", "BTC,ETH,LTC,TRX", "comma separated crypto-currencies to query")
 	physicalCurrency = flag.String("dollar", "USD", "physical currency to query value of crypto")
 )
 
 func main() {
 	flag.Parse()
 	client := av.NewClient(*apiKey)
-	fmt.Printf("querying %s price in %s...\n", *cryptoSymbol, *physicalCurrency)
-	queryCrypto(client, *cryptoSymbol, *physicalCurrency)
+	fmt.Printf("querying price in %s...\n", *physicalCurrency)
+
+	for _, symbol := range strings.Split(*cryptoSymbols, ",") {
+		queryCrypto(client, symbol, *physicalCurrency)
+	}
 }
 
 func queryCrypto(client *av.Client, digital, physical string) {
+	fmt.Printf("%s = ", digital)
 	res, err := client.DigitalCurrency(digital, physical)
 	if err != nil {
-		ExitF(2, "could not query %s => %s: %v", digital, physical, err)
+		fmt.Printf("error: %v\n", err)
+		return
 	}
 	if len(res) == 0 {
-		ExitF(3, "no records found for %s => %s", digital, physical)
+		fmt.Print("no data\n")
+		return
 	}
 	record := res[len(res)-1]
-	fmt.Printf("%s: 1 %s = %f %s\n", record.Time, digital, record.Price, physical)
+	fmt.Printf("%f %s (updated %s)\n", record.Price, physical, asLocalTime(record.Time))
 }
 
-func ErrorF(format string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, fmt.Sprintf("%s\n", format), args...)
-}
-func ExitF(code int, format string, args ...interface{}) {
-	ErrorF(format, args...)
-	os.Exit(code)
+func asLocalTime(in time.Time) string {
+	return in.In(time.Local).Format(timeFormat)
 }
